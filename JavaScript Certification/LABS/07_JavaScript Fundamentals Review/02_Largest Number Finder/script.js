a1e22c297a0aab837fe8d7022a5fc774afac2e42
Sampatee <67424398+Sampatee@users.noreply.github.com>
2026-01-02 19:52:49 +0530

    Added BTS Logic for Largest-Number-Finder(JSLabs-07-02)

--- JavaScript Certification/LABS/07_JavaScript Fundamentals Review/02_Largest Number Finder/script.js
+++ JavaScript Certification/LABS/07_JavaScript Fundamentals Review/02_Largest Number Finder/script.js
@@ -1,8 +1,29 @@
+/*
 const largestOfAll = (arrOfArrs) => {
   const arrOfMax = [];
 
   for (const arr of arrOfArrs) {
     arrOfMax.push(Math.max(...arr));
+  }
+
+  return arrOfMax;
+};
+*/
+
+//without Math.max()
+const largestOfAll = (arrOfArrs) => {
+  const arrOfMax = [];
+
+  for (const arr of arrOfArrs) {
+    let maxNum = -Infinity;
+
+    for (const num of arr) {
+      if (num > maxNum) {
+        maxNum = num;
+      }
+    }
+
+    arrOfMax.push(maxNum);
   }
 
   return arrOfMax;
