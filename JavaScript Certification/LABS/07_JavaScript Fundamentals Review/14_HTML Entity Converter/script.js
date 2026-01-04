0d32eefebd41a6ba7ff983cf5ebdc0cc08b90722
Sampatee <67424398+Sampatee@users.noreply.github.com>
2026-01-04 20:31:15 +0530

    Added JS-Lab-JS-Fundamentals-Review-14-HTML-Entity-Converter

---
+++ JavaScript Certification/LABS/07_JavaScript Fundamentals Review/14_HTML Entity Converter/script.js
@@ -0,0 +1,25 @@
+const convertHTML = (str) => {
+  const arr = str.trim().split("");
+
+  for (let i = 0; i < arr.length; i++) {
+    switch (arr[i]) {
+      case "&":
+        arr[i] = "&amp;";
+        break;
+      case "<":
+        arr[i] = "&lt;";
+        break;
+      case ">":
+        arr[i] = "&gt;";
+        break;
+      case '"':
+        arr[i] = "&quot;";
+        break;
+      case "'":
+        arr[i] = "&apos;";
+        break;
+    }
+  }
+
+  return arr.join("");
+};
